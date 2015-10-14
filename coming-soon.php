1e188e37a02f33eb83bb94567b3803ab4e4d1561
sergeyeremeev <13.eremeev.sv@gmail.com>
2015-10-14 22:40:21 +0800

    comming soon product page

---
+++ coming-soon.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Coming soon
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package _mbbasetheme
+ */
+
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <div class="coming-soon-section">
+                    <div class="coming-soon-content">
+                        <h2>Our online shop <span class="bold">is coming soon!</span></h2>
+                        <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 5 ); } ?>
+                    </div>
+                </div>
+            <?php endwhile; // end of the loop. ?>
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php get_footer(); ?>
